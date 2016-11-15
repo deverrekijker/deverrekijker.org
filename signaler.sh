1e65030745aa257b4732673fa252da83250339cb
Chadsr <ross@rchadwick.co.uk>
2016-11-15 13:32:46 +0000

    CH-CH-CH-CH-CHAANGES

--- signaler.sh
+++ signaler.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 URL="http://deverrekijker.org"
+REQ_ADDR=URL
 TIMESTAMP=$(perl -e 'print time')
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# log current time
-echo $TIMESTAMP >> "t_open.txt"
-
-# if we have internet
-if nc -zw1 google.com 80;
-then
+function send_request() {
     # local log
-    LOG=$(cat t_open.txt)
+    LOG=$(cat $DIR"t_open.txt")
 
     # send query, receive response
     SERVER_RESPONSE=$(curl --data "secret=$SECRET&data=$LOG" -s $URL/open-state/listener.php)
@@ -18,16 +16,40 @@ then
     if [ "$SERVER_RESPONSE" == "okidok" ]
     then
         # delete log
-        rm -f t_open.txt
+        rm -f $DIR"/t_open.txt"
     fi
 
     echo $TIMESTAMP - Server response: $SERVER_RESPONSE
-else
-    # there is no internet
+}
+
+function fuck() {
+    echo "[ERROR] Could not connect to the Internet"
 
     # log to connection failure log
-    echo $TIMESTAMP - Internet failure >> t_inet_fail.txt
+    echo $TIMESTAMP >> $DIR"/t_inet_fail.txt"
 
+    echo "[WARNING] Rebooting now..."
     # reboot,
     sudo shutdown -r now
-fi
+}
+
+echo "[LOG] Current directory: "$DIR
+
+# log current time
+echo $TIMESTAMP >> $DIR"/t_open.txt"
+
+#if we have internet
+echo "[LOG] Checking for Internet connection..."
+
+case "$(curl -s --max-time 2 -I "$REQ_ADDR" | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+   [23])
+       echo "[SUCCESS] HTTP connectivity is up"
+       send_request
+       ;;
+   5)
+       echo "[ERROR] The web proxy won't let us through";;
+   *)
+       echo "[ERROR] The network is down or very slow"
+       fuck
+       ;;
+esac
