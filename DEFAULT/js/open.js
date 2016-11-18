7a827e4cbfa3cbc4d2bdad8e7a53e58610e32ea9
Marvin <ross@rchadwick.co.uk>
2016-11-18 15:02:53 +0100

    lots of refactoring and style changes

--- DEFAULT/js/open.js
+++ DEFAULT/js/open.js
@@ -1,15 +1,15 @@
 
-var OPEN_COL = "#c2f0c0", CLOSED_COL = "#eebec1";
+var OPEN_COL = "rgb(132,211,107)", CLOSED_COL = "rgb(208, 68, 69)";
 
 function refreshOpenBox(is_open,last_signal){
-    
+
     document.getElementById('open-box-load').style.display = 'none';
     document.getElementById('open-text').style.display = 'inline-block';
 
-    document.getElementById('container').style.backgroundColor = (is_open==1)?OPEN_COL:CLOSED_COL;
+    document.getElementById('open-text').style.backgroundColor = (is_open==1)?OPEN_COL:CLOSED_COL;
     document.getElementById('open-text').innerHTML = (is_open)
-        ? "Currently open"
-        : "Currently closed <span style='font-size:12px'>(or troubled internet connection)</span>";
+        ? "<h2>Currently open</h2>"
+        : "<h2>Currently closed</h2><span style='font-size:12px'> (or the VU network sucks)</span>";
     document.getElementById('open-text').title = "Last signal received " + last_signal;
 }
 
