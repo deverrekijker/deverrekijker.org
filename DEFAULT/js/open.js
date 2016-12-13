b1069e620170b33d3e4c8948cfe0e8c9dc952b0f
Marvin <ross@rchadwick.co.uk>
2016-12-13 16:43:16 +0100

    Better signal messages

--- DEFAULT/js/open.js
+++ DEFAULT/js/open.js
@@ -2,6 +2,7 @@
 var OPEN_COL = "rgb(132,211,107)", CLOSED_COL = "rgb(208, 68, 69)";
 
 function refreshOpenBox(is_open,last_signal){
+    var last_rcv_msg = "Last signal: " + last_signal;
 
     document.getElementById('open-box-load').style.display = 'none';
     document.getElementById('open-text').style.display = 'inline-block';
@@ -9,8 +10,8 @@ function refreshOpenBox(is_open,last_signal){
     document.getElementById('open-text').style.backgroundColor = (is_open==1)?OPEN_COL:CLOSED_COL;
     document.getElementById('open-text').innerHTML = (is_open)
         ? "<h2>We are open!</h2><span style='font-size:12px'>(within the last 5 minutes)</span>"
-        : "<h2>We are closed!</h2><span style='font-size:12px'> (or the VU network sucks)</span>";
-    document.getElementById('open-text').title = "Last signal received " + last_signal;
+        : "<h2>We are closed!</h2><span style='font-size:12px'> (or the VU network sucks)<br><i>"+last_rcv_msg+"</i></span>";
+    document.getElementById('open-text').title = last_rcv_msg
 }
 
 var callback = function(res) {
